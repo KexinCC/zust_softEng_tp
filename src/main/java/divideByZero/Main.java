6ad5cae3c8bb0093b41e602801c1667fa3df0309
KexinCC <kevin.czm2002@gmail.com>
2023-06-10 18:46:59 +0800

    Initial commit

---
+++ src/main/java/divideByZero/Main.java
@@ -0,0 +1,13 @@
+package divideByZero;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(3/0);
+    }
+
+}
+
