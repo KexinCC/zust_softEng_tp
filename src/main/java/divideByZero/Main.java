cb5a7e92a3a77d733bc4ba1b00720c9ebf23ee0d
KexinCC <kevin.czm2002@gmail.com>
2023-06-10 18:58:22 +0800

    try/catch block added

--- src/main/java/divideByZero/Main.java
+++ src/main/java/divideByZero/Main.java
@@ -6,8 +6,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(3/0);
+        try {
+            System.out.println(3 / 0);
+        } catch (Exception e) {
+            System.err.printf("Caught runtime exception = %s\n", e);
+        }
     }
+
 
 }
 
